374141cc3fba3553c50a4cb08d1cf15b6e070562
Favour <f.kebei@alustudent.com>
2026-06-25 08:08:13 -0700

    Task 6: Build my own calculator

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if op not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = operations[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
