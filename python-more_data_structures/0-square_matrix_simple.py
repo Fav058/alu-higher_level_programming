9c341d3333d80ebd4b50062fc7ca5a05b3fddb78
Favour <f.kebei@alustudent.com>
2026-06-30 16:54:08 -0700

    Redo task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[matrix[i][j] ** 2 for j in range(len(matrix[i]))] for i in range(len(matrix))]
+    return [[value ** 2 for value in row] for row in matrix]
 
