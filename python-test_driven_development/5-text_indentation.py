06d87d5f8c544140c61d8d847055b53bc2e47b8a
Favour <f.kebei@alustudent.com>
2026-07-27 14:47:09 -0700

    Add 5-text_indentation.py and its doctest

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Module containing text_indentation."""
+"""Module that prints text with extra newlines after ., ? and :
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after ., ? and :"""
-
+    """Print text, adding two newlines after each ., ? or : character.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-
-    while i < len(text):
-        print(text[i], end="")
-
-        if text[i] in ".?:":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
+    line = ""
+    for char in text:
+        if char == " " and line == "":
             continue
-        i += 1
+        line += char
+        if char in ".?:":
+            print(line.strip())
+            print()
+            line = ""
+    if line.strip():
+        print(line.strip(), end="")
