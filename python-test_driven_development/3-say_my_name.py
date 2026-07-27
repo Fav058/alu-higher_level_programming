f43e5e97f166888873f2f845e2f4027a6c7520d2
Favour <f.kebei@alustudent.com>
2026-07-27 14:39:45 -0700

    Add 3-say_my_name.py and its doctest

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Module containing say_my_name."""
+"""Module that prints a formatted name.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a person's name."""
-
+    """Print 'My name is <first name> <last name>'.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
