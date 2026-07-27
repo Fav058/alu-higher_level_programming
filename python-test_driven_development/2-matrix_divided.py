26e161939885b5d8046d25dd41ae7576f716f28d
Favour <f.kebei@alustudent.com>
2026-07-27 14:35:52 -0700

    Add 2-matrix_divided.py and its doctest

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Module containing matrix_divided."""
+"""Module that divides all elements of a matrix by a given number.
+"""
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix."""
-
-    if (not isinstance(matrix, list) or
-            matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all(all(isinstance(x, (int, float)) for x in row)
-                    for row in matrix)):
+    """Divide all elements of a matrix, rounded to 2 decimal places.
+    """
+    if (not isinstance(matrix, list) or len(matrix) == 0 or
+            not all(isinstance(row, list) for row in matrix)):
         raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-
-    row_size = len(matrix[0])
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     for row in matrix:
-        if len(row) != row_size:
+        if not all(isinstance(n, (int, float)) and not isinstance(n, bool)
+                   for n in row):
             raise TypeError(
-                "Each row of the matrix must have the same size"
-            )
+                "matrix must be a matrix (list of lists) of integers/floats")
 
-    if not isinstance(div, (int, float)):
+    row_len = len(matrix[0])
+    if any(len(row) != row_len for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)) or isinstance(div, bool):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(num / div, 2) for num in row] for row in matrix]
+    return [[round(n / div, 2) for n in row] for row in matrix]
