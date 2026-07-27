6034bc0d584bdb600d062cb26efd88950eb27b4c
Favour <f.kebei@alustudent.com>
2026-07-27 14:44:15 -0700

    Add 4-print_square.py and its doctest

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Module containing print_square."""
+"""Module that prints a square made of the character #.
+"""
 
 
 def print_square(size):
-    """Print a square using #."""
-
-    if not isinstance(size, int):
+    """Print a square of the given size using the # character.
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
