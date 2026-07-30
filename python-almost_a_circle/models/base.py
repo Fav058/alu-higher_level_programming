79397292a42435e0508028865406063f5827d3d1
fav058 <f.kebei@alustudent.com>
2026-07-30 11:46:01 -0700

    Task 17: from_json_string static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,3 +31,10 @@ class Base:
         list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list represented by json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
