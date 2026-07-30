058cae523d266c3f0dafa76b848bc470b80e759a
fav058 <f.kebei@alustudent.com>
2026-07-30 10:49:53 -0700

    Task 1: Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base module"""
+
+
+class Base:
+    """Base class managing id attribute for all future classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
